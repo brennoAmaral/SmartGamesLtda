28d60440b7639ff625d425bb6761c6bdf40ca7dc
BrennoDev <38212111+brennoAmaral@users.noreply.github.com>
2023-12-05 14:07:38 -0300

    fix: crashed deploy

--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import { Client, Pool } from "pg";
 import { dbConfig } from "./db.config";
 
-dotenv.config({path: path.resolve(__dirname + '.env')})
+dotenv.config()
 
 const pool = new Pool(dbConfig);
 const app = express();
