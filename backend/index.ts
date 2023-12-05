ae93056f6f91529145302ecca9425c39bef4c7f8
BrennoDev <38212111+brennoAmaral@users.noreply.github.com>
2023-12-05 13:47:42 -0300

    fix: crashed deploy

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
+import * as dotenv from 'dotenv'
+
 import { Client, Pool } from "pg";
 import { dbConfig } from "./db.config";
 
-dotenv.config();
+dotenv.config({path: __dirname + '/.env'})
 
 const pool = new Pool(dbConfig);
 const app = express();
 
-// const client = new Client({
-//   connectionString:
-//     "postgres://qxpwprjdzmpcha:cd967c7d5a840ede15950370ea6449cf8ce22371e3e775d77c4fd1d80ed0e712:5432/ddq2qsdtua5ufb",
-//   ssl: { rejectUnauthorized: false },
-// });
-
-// client.connect();
 
 async function getAllGames() {
   const result = await pool.query("SELECT * FROM games")
