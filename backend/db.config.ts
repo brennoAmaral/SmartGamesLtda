7211cb9231f579aca59ae5c253f71d6db7b18e95
BrennoDev <38212111+brennoAmaral@users.noreply.github.com>
2023-12-05 12:26:31 -0300

    first commit

---
+++ backend/db.config.ts
@@ -0,0 +1,8 @@
+export const dbConfig = {
+  user: 'qxpwprjdzmpcha',
+  host: 'ec2-44-213-228-107.compute-1.amazonaws.com',
+  database: 'ddq2qsdtua5ufb',
+  password: 'cd967c7d5a840ede15950370ea6449cf8ce22371e3e775d77c4fd1d80ed0e712',
+  port: 5432,
+  ssl:{rejectUnauthorized: false},
+};
---
+++ backend/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Client, Pool } from "pg";
+import { dbConfig } from "./db.config";
+
+dotenv.config();
+
+const pool = new Pool(dbConfig);
+const app = express();
+
+// const client = new Client({
+//   connectionString:
+//     "postgres://qxpwprjdzmpcha:cd967c7d5a840ede15950370ea6449cf8ce22371e3e775d77c4fd1d80ed0e712:5432/ddq2qsdtua5ufb",
+//   ssl: { rejectUnauthorized: false },
+// });
+
+// client.connect();
+
+async function getAllGames() {
+  const result = await pool.query("SELECT * FROM games")
+  return result.rows
+}
+async function getGameById(id:number){
+  const result = await pool.query(`SELECT * FROM games WHERE id = ${id};`)
+  return result.rows
+}
+
+app.get("/getAll", async (request, response, next) => {
+  const result = await getAllGames();
+  console.log(result)
+  response.json(result);
+});
+
+app.get("/getById/:id", async (request, response)=>{
+  const result = await getGameById(parseInt(request.params.id));
+  console.log(result)
+  response.json(result);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("app runing");
+});
