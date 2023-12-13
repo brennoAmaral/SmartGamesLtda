256d22e220833fd6a7f32ea93ec7ae679d9f3338
brennoAmaral <brenno.amaral.marques@gmail.com>
2023-12-13 14:17:16 -0300

    feat: modelo relacional adicionado

--- backend/src/infra/Dump20231213.sql
+++ backend/src/infra/Dump20231213.sql
@@ -20,7 +20,7 @@ USE `smartgamesdb`;
 --
 -- Table structure for table `games`
 --
-
+  
 DROP TABLE IF EXISTS `games`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
